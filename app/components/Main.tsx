8536250f402c78b777799df92fb315d846a5ed28
TempooDev <a.bermudez.freelance@gmail.com>
2024-03-29 16:20:48 +0100

    init project

---
+++ app/components/Main.tsx
@@ -0,0 +1,456 @@
+/*
+ *  Copyright (C) 1998-2023 by Northwoods Software Corporation. All Rights Reserved.
+ */
+
+import * as go from 'gojs';
+import { produce } from 'immer';
+import * as React from 'react';
+
+import { DiagramWrapper } from './DiagramWrapper';
+import { SelectionInspector } from './SelectionInspector';
+
+import './Main.css';
+
+/**
+ * Use a linkDataArray since we'll be using a GraphLinksModel,
+ * and modelData for demonstration purposes. Note, though, that
+ * both are optional props in ReactDiagram.
+ */
+interface AppState {
+  nodeDataArray: Array<go.ObjectData>;
+  linkDataArray: Array<go.ObjectData>;
+  modelData: go.ObjectData;
+  selectedData: go.ObjectData | null;
+  skipsDiagramUpdate: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+  // Maps to store key -> arr index for quick lookups
+  private mapNodeKeyIdx: Map<go.Key, number>;
+  private mapLinkKeyIdx: Map<go.Key, number>;
+
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      nodeDataArray: [
+        {
+          key: 'Products',
+          visibility: true,
+          location: new go.Point(250, 250),
+          items: [
+            {
+              name: 'ProductID',
+              iskey: true,
+              figure: 'Decision',
+              color: 'purple',
+            },
+            {
+              name: 'ProductName',
+              iskey: false,
+              figure: 'Hexagon',
+              color: 'blue',
+            },
+            {
+              name: 'ItemDescription',
+              iskey: false,
+              figure: 'Hexagon',
+              color: 'blue',
+            },
+            {
+              name: 'WholesalePrice',
+              iskey: false,
+              figure: 'Circle',
+              color: 'green',
+            },
+            {
+              name: 'ProductPhoto',
+              iskey: false,
+              figure: 'TriangleUp',
+              color: 'red',
+            },
+          ],
+          inheriteditems: [
+            {
+              name: 'SupplierID',
+              iskey: false,
+              figure: 'Decision',
+              color: 'purple',
+            },
+            {
+              name: 'CategoryID',
+              iskey: false,
+              figure: 'Decision',
+              color: 'purple',
+            },
+          ],
+        },
+        {
+          key: 'Suppliers',
+          visibility: false,
+          location: new go.Point(500, 0),
+          items: [
+            {
+              name: 'SupplierID',
+              iskey: true,
+              figure: 'Decision',
+              color: 'purple',
+            },
+            {
+              name: 'CompanyName',
+              iskey: false,
+              figure: 'Hexagon',
+              color: 'blue',
+            },
+            {
+              name: 'ContactName',
+              iskey: false,
+              figure: 'Hexagon',
+              color: 'blue',
+            },
+            { name: 'Address', iskey: false, figure: 'Hexagon', color: 'blue' },
+            {
+              name: 'ShippingDistance',
+              iskey: false,
+              figure: 'Circle',
+              color: 'green',
+            },
+            { name: 'Logo', iskey: false, figure: 'TriangleUp', color: 'red' },
+          ],
+          inheriteditems: [],
+        },
+        {
+          key: 'Categories',
+          visibility: true,
+          location: new go.Point(0, 30),
+          items: [
+            {
+              name: 'CategoryID',
+              iskey: true,
+              figure: 'Decision',
+              color: 'purple',
+            },
+            {
+              name: 'CategoryName',
+              iskey: false,
+              figure: 'Hexagon',
+              color: 'blue',
+            },
+            {
+              name: 'Description',
+              iskey: false,
+              figure: 'Hexagon',
+              color: 'blue',
+            },
+            { name: 'Icon', iskey: false, figure: 'TriangleUp', color: 'red' },
+          ],
+          inheriteditems: [
+            {
+              name: 'SupplierID',
+              iskey: false,
+              figure: 'Decision',
+              color: 'purple',
+            },
+          ],
+        },
+        {
+          key: 'Order Details',
+          visibility: true,
+          location: new go.Point(600, 350),
+          items: [
+            {
+              name: 'OrderID',
+              iskey: true,
+              figure: 'Decision',
+              color: 'purple',
+            },
+            {
+              name: 'UnitPrice',
+              iskey: false,
+              figure: 'Circle',
+              color: 'green',
+            },
+            {
+              name: 'Quantity',
+              iskey: false,
+              figure: 'Circle',
+              color: 'green',
+            },
+            {
+              name: 'Discount',
+              iskey: false,
+              figure: 'Circle',
+              color: 'green',
+            },
+          ],
+          inheriteditems: [
+            {
+              name: 'ProductID',
+              iskey: true,
+              figure: 'Decision',
+              color: 'purple',
+            },
+          ],
+        },
+      ],
+      linkDataArray: [
+        { from: 'Products', to: 'Suppliers', text: '0..N', toText: '1' },
+        { from: 'Products', to: 'Categories', text: '0..N', toText: '1' },
+        { from: 'Order Details', to: 'Products', text: '0..N', toText: '1' },
+        { from: 'Categories', to: 'Suppliers', text: '0..N', toText: '1' },
+      ],
+      modelData: {
+        canRelink: true,
+      },
+      selectedData: null,
+      skipsDiagramUpdate: false,
+    };
+    // init maps
+    this.mapNodeKeyIdx = new Map<go.Key, number>();
+    this.mapLinkKeyIdx = new Map<go.Key, number>();
+    this.refreshNodeIndex(this.state.nodeDataArray);
+    this.refreshLinkIndex(this.state.linkDataArray);
+    // bind handler methods
+    this.handleDiagramEvent = this.handleDiagramEvent.bind(this);
+    this.handleModelChange = this.handleModelChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleRelinkChange = this.handleRelinkChange.bind(this);
+  }
+
+  /**
+   * Update map of node keys to their index in the array.
+   */
+  private refreshNodeIndex(nodeArr: Array<go.ObjectData>) {
+    this.mapNodeKeyIdx.clear();
+    nodeArr.forEach((n: go.ObjectData, idx: number) => {
+      this.mapNodeKeyIdx.set(n.key, idx);
+    });
+  }
+
+  /**
+   * Update map of link keys to their index in the array.
+   */
+  private refreshLinkIndex(linkArr: Array<go.ObjectData>) {
+    this.mapLinkKeyIdx.clear();
+    linkArr.forEach((l: go.ObjectData, idx: number) => {
+      this.mapLinkKeyIdx.set(l.key, idx);
+    });
+  }
+
+  /**
+   * Handle any relevant DiagramEvents, in this case just selection changes.
+   * On ChangedSelection, find the corresponding data and set the selectedData state.
+   * @param e a GoJS DiagramEvent
+   */
+  public handleDiagramEvent(e: go.DiagramEvent) {
+    const name = e.name;
+    switch (name) {
+      case 'ChangedSelection': {
+        const sel = e.subject.first();
+        this.setState(
+          produce((draft: AppState) => {
+            if (sel) {
+              if (sel instanceof go.Node) {
+                const idx = this.mapNodeKeyIdx.get(sel.key);
+                if (idx !== undefined && idx >= 0) {
+                  const nd = draft.nodeDataArray[idx];
+                  draft.selectedData = nd;
+                }
+              } else if (sel instanceof go.Link) {
+                const idx = this.mapLinkKeyIdx.get(sel.key);
+                if (idx !== undefined && idx >= 0) {
+                  const ld = draft.linkDataArray[idx];
+                  draft.selectedData = ld;
+                }
+              }
+            } else {
+              draft.selectedData = null;
+            }
+          })
+        );
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Handle GoJS model changes, which output an object of data changes via Model.toIncrementalData.
+   * This method iterates over those changes and updates state to keep in sync with the GoJS model.
+   * @param obj a JSON-formatted string
+   */
+  public handleModelChange(obj: go.IncrementalData) {
+    const insertedNodeKeys = obj.insertedNodeKeys;
+    const modifiedNodeData = obj.modifiedNodeData;
+    const removedNodeKeys = obj.removedNodeKeys;
+    const insertedLinkKeys = obj.insertedLinkKeys;
+    const modifiedLinkData = obj.modifiedLinkData;
+    const removedLinkKeys = obj.removedLinkKeys;
+    const modifiedModelData = obj.modelData;
+
+    // maintain maps of modified data so insertions don't need slow lookups
+    const modifiedNodeMap = new Map<go.Key, go.ObjectData>();
+    const modifiedLinkMap = new Map<go.Key, go.ObjectData>();
+    this.setState(
+      produce((draft: AppState) => {
+        let narr = draft.nodeDataArray;
+        if (modifiedNodeData) {
+          modifiedNodeData.forEach((nd: go.ObjectData) => {
+            modifiedNodeMap.set(nd.key, nd);
+            const idx = this.mapNodeKeyIdx.get(nd.key);
+            if (idx !== undefined && idx >= 0) {
+              narr[idx] = nd;
+              if (draft.selectedData && draft.selectedData.key === nd.key) {
+                draft.selectedData = nd;
+              }
+            }
+          });
+        }
+        if (insertedNodeKeys) {
+          insertedNodeKeys.forEach((key: go.Key) => {
+            const nd = modifiedNodeMap.get(key);
+            const idx = this.mapNodeKeyIdx.get(key);
+            if (nd && idx === undefined) {
+              // nodes won't be added if they already exist
+              this.mapNodeKeyIdx.set(nd.key, narr.length);
+              narr.push(nd);
+            }
+          });
+        }
+        if (removedNodeKeys) {
+          narr = narr.filter((nd: go.ObjectData) => {
+            if (removedNodeKeys.includes(nd.key)) {
+              return false;
+            }
+            return true;
+          });
+          draft.nodeDataArray = narr;
+          this.refreshNodeIndex(narr);
+        }
+
+        let larr = draft.linkDataArray;
+        if (modifiedLinkData) {
+          modifiedLinkData.forEach((ld: go.ObjectData) => {
+            modifiedLinkMap.set(ld.key, ld);
+            const idx = this.mapLinkKeyIdx.get(ld.key);
+            if (idx !== undefined && idx >= 0) {
+              larr[idx] = ld;
+              if (draft.selectedData && draft.selectedData.key === ld.key) {
+                draft.selectedData = ld;
+              }
+            }
+          });
+        }
+        if (insertedLinkKeys) {
+          insertedLinkKeys.forEach((key: go.Key) => {
+            const ld = modifiedLinkMap.get(key);
+            const idx = this.mapLinkKeyIdx.get(key);
+            if (ld && idx === undefined) {
+              // links won't be added if they already exist
+              this.mapLinkKeyIdx.set(ld.key, larr.length);
+              larr.push(ld);
+            }
+          });
+        }
+        if (removedLinkKeys) {
+          larr = larr.filter((ld: go.ObjectData) => {
+            if (removedLinkKeys.includes(ld.key)) {
+              return false;
+            }
+            return true;
+          });
+          draft.linkDataArray = larr;
+          this.refreshLinkIndex(larr);
+        }
+        // handle model data changes, for now just replacing with the supplied object
+        if (modifiedModelData) {
+          draft.modelData = modifiedModelData;
+        }
+        draft.skipsDiagramUpdate = true; // the GoJS model already knows about these updates
+      })
+    );
+  }
+
+  /**
+   * Handle inspector changes, and on input field blurs, update node/link data state.
+   * @param path the path to the property being modified
+   * @param value the new value of that property
+   * @param isBlur whether the input event was a blur, indicating the edit is complete
+   */
+  public handleInputChange(path: string, value: string, isBlur: boolean) {
+    this.setState(
+      produce((draft: AppState) => {
+        const data = draft.selectedData as go.ObjectData; // only reached if selectedData isn't null
+        data[path] = value;
+        if (isBlur) {
+          const key = data.key;
+          if (key < 0) {
+            // negative keys are links
+            const idx = this.mapLinkKeyIdx.get(key);
+            if (idx !== undefined && idx >= 0) {
+              draft.linkDataArray[idx] = data;
+              draft.skipsDiagramUpdate = false;
+            }
+          } else {
+            const idx = this.mapNodeKeyIdx.get(key);
+            if (idx !== undefined && idx >= 0) {
+              draft.nodeDataArray[idx] = data;
+              draft.skipsDiagramUpdate = false;
+            }
+          }
+        }
+      })
+    );
+  }
+
+  /**
+   * Handle changes to the checkbox on whether to allow relinking.
+   * @param e a change event from the checkbox
+   */
+  public handleRelinkChange(e: any) {
+    const target = e.target;
+    const value = target.checked;
+    this.setState({
+      modelData: { canRelink: value },
+      skipsDiagramUpdate: false,
+    });
+  }
+
+  public render() {
+    const selectedData = this.state.selectedData;
+    let inspector;
+    if (selectedData !== null) {
+      inspector = (
+        <SelectionInspector
+          selectedData={this.state.selectedData}
+          onInputChange={this.handleInputChange}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <DiagramWrapper
+          nodeDataArray={this.state.nodeDataArray}
+          linkDataArray={this.state.linkDataArray}
+          modelData={this.state.modelData}
+          skipsDiagramUpdate={this.state.skipsDiagramUpdate}
+          onDiagramEvent={this.handleDiagramEvent}
+          onModelChange={this.handleModelChange}
+        />
+        <label>
+          Allow Relinking?
+          <input
+            type="checkbox"
+            id="relink"
+            checked={this.state.modelData.canRelink}
+            onChange={this.handleRelinkChange}
+          />
+        </label>
+        {inspector}
+      </div>
+    );
+  }
+}
+
+export default App;
