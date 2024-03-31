6513a58590035382b1caebf53d2bd439cbae1ff0
TempooDev <a.bermudez.rodriguez@uma.es>
2024-03-31 19:02:56 +0200

    add login

---
+++ app/components/UserTabMenu.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+export default function UserTabMenu() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    user && (
+      <div className="dropdown dropdown-end">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src={user.picture} alt={user.name} />
+          </div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+          <li>
+            <a className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+          <li>
+            <a href="/api/auth/logout">Logout</a>
+          </li>
+        </ul>
+      </div>
+    )
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
+import { UserProvider, useUser } from "@auth0/nextjs-auth0/client";
+import UserTabMenu from "./components/UserTabMenu";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,65 +19,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="navbar bg-base-100">
-          <div className="flex-1">
-            <a className="btn btn-ghost text-xl">ERDiagrams</a>
-          </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <Link href={"/board"}>Board</Link>
-              </li>
-              <li>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2 bg-base-100 rounded-t-none">
-                    <li>
-                      <a>Link 1</a>
-                    </li>
-                    <li>
-                      <a>Link 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-            </ul>
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <Image
-                    width={"292"}
-                    height={"292"}
-                    alt="Tailwind CSS Navbar component"
-                    src="/photo.jpeg"
-                  />
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <UserProvider>
+        <body className={inter.className}>
+          <div className="navbar bg-base-100">
+            <div className="flex-1">
+              <Link className="btn btn-ghost text-xl" href={"/"}>
+                ERDiagrams
+              </Link>
+            </div>
+            <div className="flex-none">
+              <ul className="menu menu-horizontal px-1">
                 <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
+                  <Link href={"/board"}>Board</Link>
                 </li>
                 <li>
-                  <a>Settings</a>
+                  <details>
+                    <summary>Parent</summary>
+                    <ul className="p-2 bg-base-100 rounded-t-none">
+                      <li>
+                        <a>Link 1</a>
+                      </li>
+                      <li>
+                        <a>Link 2</a>
+                      </li>
+                    </ul>
+                  </details>
                 </li>
+              </ul>
+              <UserTabMenu />
+              <ul className="menu menu-horizontal px-1">
                 <li>
-                  <a>Logout</a>
+                  <a href="/api/auth/login">Login</a>
                 </li>
               </ul>
             </div>
           </div>
-        </div>
-        <div> {children}</div>
-      </body>
+
+          {children}
+        </body>
+      </UserProvider>
     </html>
   );
 }
