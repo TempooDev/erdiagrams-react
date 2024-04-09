916eb7cc5c8c538881cce54dcf627ac6303854ed
TempooDev <a.bermudez.rodriguez@uma.es>
2024-04-09 21:18:30 +0200

    inspector store

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,15 +1,23 @@
 import { combineReducers } from 'redux'
 import { diagramReducer } from './diagram/reducers'
 import { DiagramState } from './diagram/types';
+import { InspectorState } from './inspector/types';
+import { inspect } from 'util';
+import { inspectorReducer } from './inspector/reducers';
+import { createStore } from '@reduxjs/toolkit';
 
 export interface AppState {
     diagram: DiagramState;
+    inspector: InspectorState;
 }
 
 
 const rootReducer = combineReducers({
-    diagram: diagramReducer
+    diagram: diagramReducer,
+    inspector: inspectorReducer
 });
 
 export default function configureStore() {
+    const store = createStore(rootReducer, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
+    return store;
 }
