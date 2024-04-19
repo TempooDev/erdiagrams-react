6821fa34422e8068be848b07cdf244f7adfdb79f
TempooDev <a.bermudez.freelance@gmail.com>
2024-04-19 21:14:28 +0200

    signalr config

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as signalR from '@microsoft/signalr';
 
 interface PageState {
@@ -11,34 +11,83 @@ interface PageState {
 interface PageProp {
   params: PageState;
 }
-export default function Home({ params }: PageProp) {
-  params = {
-    user: '',
-    message: '',
-    messages: [],
-    hubConnection: null,
+
+type Message = {
+  user: string;
+  content: string;
+};
+
+export default function Chat({ params }: PageProp) {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [user, setUser] = useState('yo');
+  const [message, setMessage] = useState('');
+  const hubConnection = useRef<signalR.HubConnection | null>(null);
+
+  useEffect(() => {
+    const createHubConnection = async () => {
+      const hubConnect = new signalR.HubConnectionBuilder()
+        .withUrl('https://localhost:5010/hub/chat')
+        .withAutomaticReconnect()
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+      try {
+        await hubConnect.start();
+        console.log('Connection successful!');
+        hubConnect.on('ReceiveMessage', (user: string, message: string) => {
+          setMessages((messages) => [...messages, { user, content: message }]);
+        });
+        await hubConnect.invoke('RetrieveMessageHistory');
+      } catch (err) {
+        alert('Error establishing connection! Attempting to reconnect...');
+      }
+
+      hubConnection.current = hubConnect;
+    };
+
+    createHubConnection();
+
+    return () => {
+      hubConnection.current?.off('ReceiveMessage');
+    };
+  }, []);
+  const handleSendMessage = async () => {
+    if (
+      hubConnection.current &&
+      hubConnection.current.state === signalR.HubConnectionState.Connected &&
+      message.trim()
+    ) {
+      await hubConnection.current.send('SendMessage', user, message);
+      setMessage(''); //reset message
+    } else {
+      console.log('Connection not established');
+    }
   };
 
-  const hubConnection = new signalR.HubConnectionBuilder()
-    .withUrl('http://localhost:5010/hub/board')
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
-
-  hubConnection
-    .start()
-    .then(() => {})
-    .catch((err) => console.error(err.toString()));
-  hubConnection.on('JoinGroup', (message) => {
-    console.log('JoinGroup', message);
-  });
-
-  const handleSendMessage = (message: string) => {};
-
   return (
-    <div>
-      <input type="text" placeholder="Type your message" />
-      <input type="text" placeholder="User" />
-      <button onClick={() => handleSendMessage('Hello!')}>Send</button>
+    <div className="p-4">
+      <div className="mb-4">
+        {messages.map((msg, index) => (
+          <div key={index} className={`p-2 my-2 rounded`}>
+            <p>{msg.content}</p>
+          </div>
+        ))}
+      </div>
+      <div className="d-flex justify-row">
+        <input
+          type="text"
+          className="border p-2 mr-2 rounded w-[300px]"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <button
+          onClick={handleSendMessage}
+          className="bg-blue-500 text-white p-2 rounded"
+        >
+          Send
+        </button>
+      </div>
     </div>
   );
 }
