d09b0c7d97cb69e022f0baae2f32bf7f0afeebad
TempooDev <a.bermudez.freelance@gmail.com>
2024-04-26 17:19:22 +0200

    loading

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -21,17 +21,16 @@ type Message = {
 export default function Chat({ params }: PageProp) {
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState('');
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState('');
   const [connection, setConnection] = useState<signalR.HubConnection | null>(
     null
   );
   const apiURL = process.env.NEXT_PUBLIC_CHAT_URL!;
   useEffect(() => {
-    const connect = new signalR.HubConnectionBuilder()
-      .withUrl(apiURL)
-      .withAutomaticReconnect()
-      .configureLogging(signalR.LogLevel.Information)
-      .build();
+    const connect = new signalR.HubConnectionBuilder().withUrl(apiURL).build();
+
+    if (connect.state) setLoading(false);
     setConnection(connect);
     connect
       .start()
@@ -40,18 +39,20 @@ export default function Chat({ params }: PageProp) {
           setMessages((prev) => [...prev, { sender, content, sentTime }]);
         });
         connect.invoke('RetrieveMessageHistory');
+        setLoading(false);
       })
-
-      .catch((err) =>
-        console.error('Error while connecting to SignalR Hub:', err)
-      );
+      .catch((err) => {
+        setLoading(true);
+        console.error('Error while connecting to SignalR Hub:', err);
+      });
 
     return () => {
       if (connection) {
         connection.off('ReceiveMessage');
       }
     };
-  }, [apiURL]);
+  }, []);
+
   const sendMessage = async () => {
     if (connection && newMessage.trim()) {
       await connection.send('PostMessage', newMessage);
@@ -62,44 +63,51 @@ export default function Chat({ params }: PageProp) {
     return connection && username === connection.connectionId;
   };
 
-  return (
-    <div className="p-4">
-      <div className="mb-4">
-        {messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`p-2 my-2 rounded ${
-              isMyMessage(msg.sender) ? 'bg-blue-900' : 'bg-gray-200'
-            }`}
-          >
-            <p>{msg.content}</p>
-            <p className="text-xs">{new Date(msg.sentTime).toLocaleString()}</p>
-          </div>
-        ))}
-      </div>
-      <div className="d-flex justify-row">
-        <input
-          type="text"
-          className="border p-2 mr-2 rounded w-[300px]"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Enter your message"
-        />
+  if (loading) {
+    return <span className="loading loading-spinner loading-lg"></span>;
+  } else {
+    return (
+      <div className="p-4">
+        <div className="mb-4">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`p-2 my-2 rounded text-black ${
+                isMyMessage(msg.sender) ? 'bg-blue-900' : 'bg-gray-100'
+              }`}
+            >
+              <p>{msg.content}</p>
+              <p>{msg.sender}</p>
+              <p className="text-xs">
+                {new Date(msg.sentTime).toLocaleString()}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="d-flex justify-row">
+          <input
+            type="text"
+            className="border p-2 mr-2 rounded w-[300px]"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Enter your message"
+          />
 
-        <input
-          type="text"
-          className="border p-2 mr-2 rounded w-[300px]"
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
-          placeholder="Enter your username"
-        />
-        <button
-          onClick={sendMessage}
-          className="bg-blue-500 text-white p-2 rounded"
-        >
-          Send
-        </button>
+          <input
+            type="text"
+            className="border p-2 mr-2 rounded w-[300px]"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            placeholder="Enter your username"
+          />
+          <button
+            onClick={sendMessage}
+            className="bg-blue-500 text-white p-2 rounded"
+          >
+            Send
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
