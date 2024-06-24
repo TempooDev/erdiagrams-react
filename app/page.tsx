8ef840ba1b8a7ad086cd46e239b103ff0ed2a477
TempooDev <a.bermudez.rodriguez@uma.es>
2024-06-24 21:59:03 +0200

    feat: show all diagrams

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import Image from "next/image";
-import diagrams from "./mocks/diagrams";
+import Image from 'next/image';
+import diagrams from './mocks/diagrams';
 
-import Link from "next/link";
+import Link from 'next/link';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { Diagram } from './store/diagram/types';
 
-export default function Home() {
+export const getServerSideProps = (async () => {
+  // Fetch data from external API
+  const res = await fetch('https://api-erdiagrams.azurewebsites.net/diagrams');
+  const diagrams: Diagram[] = await res.json();
+  // Pass data to the page via props
+  return { props: { diagrams } };
+}) satisfies GetServerSideProps<{ diagrams: Diagram[] }>;
+
+export default function Page({
+  diagrams,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="flex justify-center place-content-center">
-      {diagrams.map((diagram, id) => (
+      {/* {diagrams.map((diagram, id) => (
         <div
           className="card card-compact w-96 bg-base-50 shadow-xl m-2"
-          key={id}>
+          key={id}
+        >
           <figure>
             <Image
               src="/diagram1.png"
@@ -26,12 +39,13 @@ export default function Home() {
             </p>
             <Link
               href={`/board/${diagram.id}`}
-              className="card-actions justify-end">
+              className="card-actions justify-end"
+            >
               <button className="btn btn-primary">Abrir</button>
             </Link>
           </div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
