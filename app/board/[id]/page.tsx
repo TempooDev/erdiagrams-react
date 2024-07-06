e43bfe2c79cf63261fb12681e274250e3e2a4324
TempooDev <a.bermudez.rodriguez@uma.es>
2024-07-06 20:27:11 +0200

    feat: add signalR

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Board from '@/app/components/board/Board';
 import { Diagram } from '@/app/store/diagram/types';
+import signalR, { HubConnectionBuilder } from '@microsoft/signalr';
 import { useEffect, useState } from 'react';
 
 interface HomeProps {
@@ -12,11 +13,17 @@ interface HomeProps {
 export default function Home({ params }: HomeProps) {
   const [diagram, setDiagram] = useState<Diagram | undefined>(undefined);
   const [isLoading, setLoading] = useState(true);
-  const [first, setFirst] = useState(true);
+
+  const [connection, setConnection] = useState<signalR.HubConnection | null>(
+    null
+  );
+  const apiURL = 'https://api-erdiagrams.azurewebsites.net/hub/board';
+
   useEffect(() => {
-    if (!params.id) return;
-    if (!isLoading) return;
-    if (first) {
+    const fetchData = async () => {
+      if (!params.id) return;
+      if (!isLoading) return;
+
       fetch('https://api-erdiagrams.azurewebsites.net/diagrams/' + params.id)
         .then((res) => res.json())
         .then((data) => {
@@ -24,9 +31,35 @@ export default function Home({ params }: HomeProps) {
           setLoading(false);
         });
       console.log(diagram);
-      setFirst(false);
-    }
-  }, []);
+    };
+
+    const initSignalRConnection = async () => {
+      const connection = new HubConnectionBuilder()
+        .withUrl(apiURL)
+        .withAutomaticReconnect()
+        .build();
+
+      try {
+        await connection.start();
+        console.log('SignalR Connected');
+        setConnection(connection);
+        connection.on('ReceiveMessage', (message) => {
+          console.log('Message received:', message);
+          // Actualiza el estado o maneja el mensaje como sea necesario
+        });
+      } catch (error) {
+        console.error('SignalR Connection Error:', error);
+      }
+    };
+
+    fetchData();
+    initSignalRConnection();
+
+    return () => {
+      connection?.stop().then(() => console.log('SignalR Disconnected'));
+    };
+  }, [params.id]); // Asegúrate de incluir params.id si su valor puede cambiar y afectar la lógica de carga
+
   if (isLoading) return <>Loading....</>;
 
   if (diagram) return <Board diagram={diagram}></Board>;
